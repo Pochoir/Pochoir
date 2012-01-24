a3e3ec5604d8fed7a9b7eb7a68d816c2aeb1cb04
Yuan Tang <yuantang@csail.mit.edu>
2012-01-24 19:39:16 +0000

    progress on 20120124 2:40pm

--- pochoir_kernel.hpp
+++ pochoir_kernel.hpp
@@ -34,12 +34,12 @@ using namespace std;
 template <int N_RANK>
 struct Pochoir_Kernel {
     typedef typename Pochoir_Types<N_RANK>::T_Kernel T_Kernel;
-    T_Kernel & kernel_;
+    T_Kernel * kernel_;
     Pochoir_Shape<N_RANK> * shape_;
     int shape_size_, time_shift_, toggle_, slope_[N_RANK];
     Pochoir_Kernel(void) { }
     template <int N_SIZE>
-    Pochoir_Kernel(T_Kernel & _kernel, Pochoir_Shape<N_RANK> (& _shape)[N_SIZE]) : kernel_(_kernel), shape_(_shape), shape_size_(N_SIZE) {
+    Pochoir_Kernel(T_Kernel _kernel, Pochoir_Shape<N_RANK> (& _shape)[N_SIZE]) : kernel_(&_kernel) {
         int l_min_time_shift=0, l_max_time_shift=0, depth=0;
         for (int r = 0; r < N_RANK+1; ++r) {
             slope_[r] = 0;
@@ -64,22 +64,22 @@ struct Pochoir_Kernel {
     }
     Pochoir_Shape<N_RANK> * Get_Shape() { return shape_; }
     int Get_Shape_Size() { return shape_size_; }
-    T_Kernel & Get_Kernel(void) { return kernel_; }
+    T_Kernel & Get_Kernel(void) { return (*kernel_); }
     template <typename ... IS>
-    inline void operator() (int t, IS ... is) const { kernel_(t, is ...); }
+    inline void operator() (int t, IS ... is) const { (*kernel_)(t, is ...); }
 };
 
 /* Pochoir_Obase_Kernel for Phase II */
 template <int N_RANK>
 struct Pochoir_Obase_Kernel {
     typedef typename Pochoir_Types<N_RANK>::T_Obase_Kernel T_Kernel;
-    T_Kernel & kernel_;
+    T_Kernel * kernel_;
     Pochoir_Shape<N_RANK> * shape_;
     int shape_size_, time_shift_, toggle_, slope_[N_RANK];
     Pochoir_Obase_Kernel(void) { }
     template <int N_SIZE>
-    int Init(T_Kernel & _kernel, Pochoir_Shape<N_RANK> (& _shape)[N_SIZE]) {
-        kernel_ = _kernel; shape_ = _shape; shape_size_ = N_SIZE;
+    int Init(T_Kernel _kernel, Pochoir_Shape<N_RANK> (& _shape)[N_SIZE]) {
+        kernel_ = &_kernel; 
         int l_min_time_shift=0, l_max_time_shift=0, depth=0;
         for (int r = 0; r < N_RANK+1; ++r) {
             slope_[r] = 0;
@@ -115,7 +115,7 @@ struct Pochoir_Obase_Kernel {
         return 0;
     }
     template <int N_SIZE>
-    Pochoir_Obase_Kernel(T_Kernel & _kernel, Pochoir_Shape<N_RANK> (& _shape)[N_SIZE]) : kernel_(_kernel), shape_(_shape), shape_size_(N_SIZE) {
+    Pochoir_Obase_Kernel(T_Kernel _kernel, Pochoir_Shape<N_RANK> (& _shape)[N_SIZE]) : kernel_(&_kernel) {
         int l_min_time_shift=0, l_max_time_shift=0, depth=0;
         for (int r = 0; r < N_RANK+1; ++r) {
             slope_[r] = 0;
@@ -151,21 +151,21 @@ struct Pochoir_Obase_Kernel {
     }
     Pochoir_Shape<N_RANK> * Get_Shape() { return shape_; }
     int Get_Shape_Size() { return shape_size_; }
-    T_Kernel & Get_Kernel (void) { return kernel_; }
+    T_Kernel & Get_Kernel (void) { return (*kernel_); }
     inline void operator() (int t0, int t1, Grid_Info<N_RANK> const & grid) const {
-        kernel_(t0, t1, grid);
+        (*kernel_)(t0, t1, grid);
     }
 };
 
 template <int N_RANK>
 struct Pochoir_Guard {
     typedef typename Pochoir_Types<N_RANK>::T_Guard T_Guard;
-    T_Guard & guard_;
+    T_Guard * guard_;
     Pochoir_Guard(void) { }
-    Pochoir_Guard(T_Guard _guard) : guard_(_guard) { }
+    Pochoir_Guard(T_Guard _guard) : guard_(&_guard) { }
     template <typename ... IS>
-    inline bool operator() (int t, IS ... is) const { return guard_(t, is ...); }
-    T_Guard & Get_Guard (void) { return guard_; }
+    inline bool operator() (int t, IS ... is) const { return (*guard_)(t, is ...); }
+    T_Guard & Get_Guard (void) { return (*guard_); }
 };
 
 #define Pochoir_Guard_3D_Begin(name, t, i, j, k) \
