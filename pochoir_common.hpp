00ec871433ad9197268f3f176e930033a3eeb31d
Eka Palamadai <epn@mit.edu>
2012-06-13 17:48:08 +0000

    Removed creating plans in preprocessing time and using plans at runtime

--- pochoir_common.hpp
+++ pochoir_common.hpp
@@ -66,6 +66,9 @@ static inline int lcm(int a, int b) {
 
 #define ARRAY_LENGTH(x) (int)(sizeof(x)/sizeof(x[0]))
 
+//typedef for rbq
+typedef unsigned long POCHOIR_WORD_TYPE ;
+
 #if 0
 #define cilk_for for
 #define cilk_spawn 
