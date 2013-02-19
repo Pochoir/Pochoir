c6f595cb066524f10472b73e2eacefa69fbc77e7
Angelina Lee <angelee@gmail.com>
2013-02-19 14:34:32 -0500

    Removing extra files accidentally copied

--- Scripts/run_2D_spaa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-size_low=100
-size_up=12800
-
-for ((size = ${size_low}; size <= ${size_up}; size += ${size})) do
-	echo "$1 $size 1000"
-	$1 $size 1000
-done
--- Scripts/scal_2D.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-cilkview ./heat_duo 2000 2000 >& duo.scal
-
-cilkview ./heat_sim 2000 2000 >& sim.scal
-set +x
-
