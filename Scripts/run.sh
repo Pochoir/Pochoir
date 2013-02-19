faa8b022609f33427eabccdb244b0a4b85942d2f
Angelina Lee <angelee@gmail.com>
2013-02-19 14:34:32 -0500

    Removing extra files accidentally copied

--- Scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-
-./run_cmp.sh
-./run_perf.sh
-./run_scal.sh
-
-set +x
--- Scripts/run_4D_spaa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-size_low=10
-size_up=160
-
-for ((size = ${size_low}; size <= ${size_up}; size += $size)) do
-	echo "$1 $size $size" 
-	$1 $size $size
-done
