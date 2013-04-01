8692c228c42fa4d2f38937dbf697b06902cf6eaf
Rodrigo Munoz <rodmk@heliox.csail.mit.edu>
2013-04-01 04:11:37 -0400

    Updated the Makefile to allow easy make-ing of all examples
    This diff addresses issue #1 and partially #2. It permits one to simply run
    "make" and have all the examples automatically compile. One can also run "make
    stencil_name" for the stencil "tb_stencil_name.cpp" to make said stencil.
    
    Running "make check" allows one to run an extra set of additional checks,
    though currently these are simply existence check to ensure that the examples
    compile properly.
    
    Running "make DEBUG_POCHOIR=1" and "make DEBUG_ICC=1" makes the examples in
    debug and icc debug mode, respectively. Similarly, you can run "make check
    DEBUG_POCHOIR=1" and "make check DEBUG_ICC=1" to run the test suite on these
    examples.

---
+++ examples/check.py
@@ -0,0 +1,46 @@
+# Tests
+import subprocess
+import sys
+import re
+import glob
+import os
+import re
+
+def green(s):
+    return '\033[1;32m%s\033[m' % s
+
+def red(s):
+    return '\033[1;31m%s\033[m' % s
+
+def log(*m):
+    print >> sys.stderr, " ".join(m)
+
+
+def check(name):
+    if check_existence(name) and check_correctness(name):
+        log(green("PASS"), "%s correctness check" % name)
+
+def check_existence(name):
+    try:
+        with open(name):
+            return True
+    except IOError:
+        log(red("FAIL"), "%s not found" % name)
+        return False
+
+def check_correctness(name):
+    return True
+
+if __name__ == "__main__":
+    stencils = [os.path.splitext(filename)[0] for filename in glob.glob('*.cpp') if "tb_" in filename]
+    stencils = set([re.sub("_pochoir", "", stencil) for stencil in stencils])
+    stencils = set([re.sub("_kernel_info", "", stencil) for stencil in stencils])
+    stencils = set([re.sub("tb_", "", stencil) for stencil in stencils])
+
+    if "DEBUG_POCHOIR" in sys.argv:
+        stencils = [stencil + "_pochoir" for stencil in stencils]
+    elif "DEBUG_ICC" in sys.argv:
+        stencils = [stencil + "_gdb" for stencil in stencils]
+
+    for stencil in sorted(stencils):
+        check(stencil)
