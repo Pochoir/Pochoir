359ddea2123c3d119e402a8d1f05f9a272dfd8b1
Eka Palamdai <epn@mit.edu>
2014-04-16 06:47:47 -0400

    revised

--- examples/copy_benchmarks.sh
+++ examples/copy_benchmarks.sh
@@ -1,5 +1,5 @@
 #declare -a arr=("element1" "element2" "element3")
-declare -a CASES=("heat_2D_NP" "heat_2D_P" "3dfd" "heat_3D_NP" "heat_4D_NP" "life" "rna" "psa_struct" "lcs" "apop" "LBM/BIN/lbm_tang")
+declare -a CASES=("heat_1D_NP" "heat_2D_NP" "heat_2D_P" "3dfd" "heat_3D_NP" "heat_4D_NP" "life" "rna" "psa_struct" "lcs" "apop" "LBM/BIN/lbm_tang")
 ## now loop through the above array
 for i in "${CASES[@]}"
 do
